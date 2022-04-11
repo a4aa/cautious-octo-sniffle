99ecfcf1b1f9ac52859ef802caac322063dfc91d
a4aa <79127903+a4aa@users.noreply.github.com>
2022-04-11 11:58:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA74oKwuKzO3W4n47MnXQkZMCmg7Valcwc",
+    authDomain: "storytellingapp-b37c9.firebaseapp.com",
+    projectId: "storytellingapp-b37c9",
+    storageBucket: "storytellingapp-b37c9.appspot.com",
+    messagingSenderId: "478340421744",
+    appId: "1:478340421744:web:399ab5af5a44569db97cb5"
+  };
